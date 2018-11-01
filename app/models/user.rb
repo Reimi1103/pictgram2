729caa675e2124ee9e816c9020865199b6e28f92
レイミ <reimi@shin-MacBook-Air.local>
2018-11-01 20:00:47 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_secure_password
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{3,8}+\z/i}
+
+  has_many :topics
+  
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
+  has_many :comments
+end
---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  get 'topics/new'
+  get 'sessions/new'
+  root 'pages#index'
+  get 'pages/help'
+
+  resources :users
+  resources :topics
+  resources :comments
+
+
+
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get    'favorites/index'
+  post   '/favorites', to: 'favorites#create'
+
+
+end
